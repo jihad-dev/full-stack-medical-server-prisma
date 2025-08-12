19e2fffa9797d95bb8cfb95858d2b660eb3fe9fe
jihad-dev <mdjihadislam166@gmail.com>
2025-08-12 21:49:27 +0600

    pagination-added

--- src/app/modules/Schedule/schedule.controller.ts
+++ src/app/modules/Schedule/schedule.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { sendResponse } from "../../../Shared/sendResponse";
 import { catchAsync } from "../../middlewares/catchAsync";
 import { scheduleServices } from "./schedule.service";
+import pick from "../../../Shared/pick";
+import { scheduleFilterableFields } from "./schedule.constant";
 
 const createSchedule = catchAsync(async (req: Request, res: Response) => {
   const result = await scheduleServices.createSchedule(req.body);
@@ -12,16 +14,20 @@ const createSchedule = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-// const getAllPatient = catchAsync(async (req: Request, res: Response) => {
-//   const result = await patientServices.getAllPatient();
-//   sendResponse(res, {
-//     statusCode: 200,
-//     success: true,
-//     message: "Patient Data Get successfully",
-//     data: result,
-//   });
-// });
+
+const getAllSchedule = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, scheduleFilterableFields);
+  const options = pick(req.query, ["page", "limit", "sortOrder", "sortBy"]);
+  const result = await scheduleServices.getAllSchedule(filters, options);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Schedule retrieved successfully",
+    data: result,
+  });
+});
 
 export const scheduleController = {
   createSchedule,
+  getAllSchedule
 };
--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,132 +1,60 @@
 import { addHours, addMinutes, format, parse } from "date-fns";
 import { prisma } from "../../../Shared/prisma";
 import { SchedulePayload } from "./schedule.interface";
-import { Schedule } from "../../../generated/prisma";
+import { Prisma, Schedule } from "../../../generated/prisma";
+import { paginationHelper } from "../../../helpers/pagination";
 
-// const getSinglePatient = async (id: string) => {
-//   const result = await prisma.patient.findUniqueOrThrow({
-//     where: {
-//       id,
-//     },
-//     include: {
-//       patientHealthData: true,
-//       medicalReport: true,
-//     },
-//   });
-//   return result;
-// };
-// const HardDeletePatient = async (id: string) => {
-//   const result = await prisma.$transaction(async (tx) => {
-//     // Step 1: patient info
-//     const patient = await tx.patient.findUniqueOrThrow({
-//       where: { id },
-//     });
-//     // Step 2: Delete  medicalReport
-//     await tx.medicalReport.deleteMany({
-//       where: { patientId: id },
-//     });
-//     // Step 3: Delete patientHealthData
-//     await tx.patientHealthData.deleteMany({
-//       where: { patientId: id },
-//     });
+const getAllSchedule = async (
+  params:any ,
+  options: any
+) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = params;
+  console.log(filterData);
+  
+  const andCondions: Prisma.ScheduleWhereInput[] = [];
+  // Exact Match
+  if (Object.keys(filterData).length > 0) {
+    andCondions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+  const whereConditons: Prisma.ScheduleWhereInput = { AND: andCondions };
 
-//     // Step 4: Delete patient
-//     await tx.patient.delete({
-//       where: { id },
-//     });
+  const result = await prisma.schedule.findMany({
+    where: whereConditons,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+  });
 
-//     // Step 5: Delete user
-//     const patientData = await tx.user.delete({
-//       where: { email: patient.email },
-//     });
+  const total = await prisma.schedule.count({
+    where: whereConditons,
+  });
 
-//     return patientData;
-//   });
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
 
-//   return result;
-// };
 
-// const SoftdeletePatient = async (id: string): Promise<Patient | null> => {
-//   return await prisma.$transaction(async (tx) => {
-//     const patient = await tx.patient.findUniqueOrThrow({
-//       where: { id },
-//     });
 
-//     await tx.patient.update({
-//       where: { id },
-//       data: { isDeleted: true },
-//     });
-
-//     await tx.user.update({
-//       where: { email: patient.email },
-//       data: { status: userStatus.BLOCKED },
-//     });
-
-//     return tx.patient.findUnique({ where: { id } });
-//   });
-// };
-
-// const updatePatientInfo = async (
-//   id: string,
-//   payload: Partial<IPatientUpdate>
-// ): Promise<Patient | null> => {
-//   const { patientHealthData, medicalReport, ...patientData } = payload;
-
-//   try {
-//     // 1️⃣ Check if patient exists
-//     const patientInfo = await prisma.patient.findUniqueOrThrow({
-//       where: {
-//         id,
-//         isDeleted: false,
-//       },
-//     });
-
-//     // 2️⃣ Transaction
-//     const result = await prisma.$transaction(async (tx) => {
-//       // 2.1 Update patient main data
-//       await tx.patient.update({
-//         where: { id },
-//         data: patientData,
-//       });
-
-//       // 2.2 Create or update patient health data
-//       if (patientHealthData) {
-//         await tx.patientHealthData.upsert({
-//           where: { patientId: id },
-//           create: {
-//             patientId: id,
-//             ...patientHealthData,
-//           },
-//           update: patientHealthData,
-//         });
-//       }
-
-//       // 2.3 Create medical report (optional)
-//       if (medicalReport) {
-//         await tx.medicalReport.create({
-//           data: { ...medicalReport, patientId: id },
-//         });
-//       }
-
-//       // 2.4 Return updated patient with relations (use tx)
-//       return await tx.patient.findUnique({
-//         where: { id: patientInfo.id },
-//         include: {
-//           patientHealthData: true,
-//           medicalReport: true,
-//         },
-//       });
-//     });
-
-//     // ✅ Return the transaction result
-//     return result;
-//   } catch (error: any) {
-//     if (error.code === "P2025") {
-//       throw new AppError(404, "Patient not found");
-//     }
-//     throw error;
-//   }
-// };
 const createSchedule = async (
   payload: SchedulePayload
 ): Promise<Schedule[]> => {
@@ -182,9 +110,5 @@ const createSchedule = async (
 };
 export const scheduleServices = {
   createSchedule,
-  //   getAllPatient,
-  //   getSinglePatient,
-  //   updatePatientInfo,
-  //   SoftdeletePatient,
-  //   HardDeletePatient,
+ getAllSchedule
 };
